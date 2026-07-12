fd437744cb919c8017cad8aa6461a6e197757c26
SEPILBOARD <sepilboard@gmail.com>
2026-07-12 19:08:08 +0900

    before 0712

--- devices/timer.c
+++ devices/timer.c
@@ -8,6 +8,8 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 
+#include <list.h>
+
 /* See [8254] for hardware details of the 8254 timer chip. */
 
 #if TIMER_FREQ < 19
@@ -23,6 +25,8 @@ static int64_t ticks;
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
+
+static struct list sleep_list;
 
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
@@ -43,6 +47,8 @@ timer_init (void) {
 	outb (0x40, count >> 8);
 
 	intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+
+	list_init(&sleep_list);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
@@ -93,8 +99,13 @@ timer_sleep (int64_t ticks) {
 	int64_t start = timer_ticks ();
 
 	ASSERT (intr_get_level () == INTR_ON);
-	while (timer_elapsed (start) < ticks)
-		thread_yield ();
+	// while (timer_elapsed (start) < ticks)
+	// 	thread_yield ();
+
+	struct thread *cur = thread_current();
+	cur->wakeup_tick = timer_ticks () + ticks;
+	list_insert(list_end(&sleep_list), &cur->elem);
+	thread_block();
 }
 
 /* Suspends execution for approximately MS milliseconds. */
@@ -126,6 +137,20 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
 	thread_tick ();
+
+	struct list_elem *e = list_begin(&sleep_list);
+
+    while(e != list_end(&sleep_list)){
+        struct thread *t = list_entry(e, struct thread, elem);
+
+        if(t->wakeup_tick <= ticks){
+            e = list_remove(e);
+            thread_unblock(t);
+        }
+        else{
+            e = list_next(e);
+        }
+    }
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
--- include/threads/thread.h
+++ include/threads/thread.h
@@ -92,6 +92,8 @@ struct thread {
 	char name[16];                      /* Name (for debugging purposes). */
 	int priority;                       /* Priority. */
 
+	int wakeup_tick;
+
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
 
